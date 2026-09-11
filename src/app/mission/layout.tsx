185ea4cc4a9bbdfe9d482da9c0f1e2000c0cf334
Naaf2026 <massakathmv@gmail.com>
2026-09-11 23:28:03 +0500

    feat: mount mission progression map

--- src/app/mission/layout.tsx
+++ src/app/mission/layout.tsx
@@ -1,10 +1,12 @@
 import GameMechanicsSystem from "@/components/game-mechanics-system";
+import MissionProgressMap from "@/components/mission-progress-map";
 import RewardsPersistenceSystem from "@/components/rewards-persistence-system";
 
 export default function MissionLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
+      <MissionProgressMap />
       <GameMechanicsSystem />
       <RewardsPersistenceSystem />
     </>
