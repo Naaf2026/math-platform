dadb478e4591acf23a008d8375759e6080a5b29e
Naaf2026 <massakathmv@gmail.com>
2026-09-12 00:00:28 +0500

    feat: mount KooBits-style mission question popup

--- src/app/mission/layout.tsx
+++ src/app/mission/layout.tsx
@@ -2,6 +2,7 @@ import GameMechanicsSystem from "@/components/game-mechanics-system";
 import MissionAnswerBridge from "@/components/mission-answer-bridge";
 import MissionCompletionSystem from "@/components/mission-completion-system";
 import MissionProgressMap from "@/components/mission-progress-map";
+import MissionQuestionPopup from "@/components/mission-question-popup";
 import RewardsPersistenceSystem from "@/components/rewards-persistence-system";
 
 export default function MissionLayout({ children }: { children: React.ReactNode }) {
@@ -9,6 +10,7 @@ export default function MissionLayout({ children }: { children: React.ReactNode 
     <>
       {children}
       <MissionProgressMap />
+      <MissionQuestionPopup />
       <MissionAnswerBridge />
       <MissionCompletionSystem />
       <GameMechanicsSystem />
