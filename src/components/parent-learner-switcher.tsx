12610d92ae48dc149edb8749625d961aae22faf0
Naaf2026 <massakathmv@gmail.com>
2026-09-12 16:26:30 +0500

    Polish parent learner switcher

--- src/components/parent-learner-switcher.tsx
+++ src/components/parent-learner-switcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Check, ChevronDown } from "lucide-react";
+import { Check } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { getMyLearners, type LearnerAccount } from "@/lib/parent-learners";
 
