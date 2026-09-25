27fdb57009fbf3f6a72cd8ac64afbbd2662467aa
Naaf2026 <massakathmv@gmail.com>
2026-09-25 13:39:01 +0500

    Add Ministry of Education school directory for learner enrollment

---
+++ src/lib/maldives-schools.ts
@@ -0,0 +1,330 @@
+// Ministry of Education public school directory, updated 23 May 2025.
+// Source: https://www.moe.gov.mv/en/schools
+// Includes government schools listed there; use “Other school” for unlisted private schools.
+export const maldivesSchoolGroups = [
+  {
+    "atoll": "Haa Alifu",
+    "schools": [
+      "Sheikh Ibrahim School",
+      "Maarandhoo School",
+      "Shaheed Ali Thakurufaanu School",
+      "Thuraakunu School",
+      "Ihavandhoo School",
+      "Filladhoo School",
+      "Vashafaru School",
+      "Baarashu School",
+      "Ha. Atoll Education Centre",
+      "Ha. Atoll school",
+      "Molhadhoo School",
+      "Uligamu School",
+      "Muraidhoo School",
+      "Ghaazee Bandaarain School"
+    ]
+  },
+  {
+    "atoll": "Haa Dhaalu",
+    "schools": [
+      "Kumundhoo School",
+      "Finey School",
+      "Hanimaadhoo School",
+      "Naivaadhoo School",
+      "Nolhivaranfaru School",
+      "Nellaidhoo School",
+      "Nolhivaram School",
+      "Kurinbee School",
+      "Neykurendhoo School",
+      "Makunudhoo School",
+      "Hirimaradhoo School",
+      "Hdh. Atoll Education Centre",
+      "Hdh. Atoll School",
+      "Jalaaludin school",
+      "Afeefudin School"
+    ]
+  },
+  {
+    "atoll": "Shaviyani",
+    "schools": [
+      "Milandhoo School",
+      "Maaungoodhoo School",
+      "Funadhoo School",
+      "Munavvara School",
+      "Feevaku School",
+      "Narudhoo School",
+      "Sh. Goidhoo School",
+      "Lhaimagu School",
+      "Bileffahee School",
+      "Foakaidhoo School",
+      "Noomaraa School",
+      "Sh. Feydhoo School",
+      "Sh. Atoll Education Centre",
+      "Sh. Atoll School"
+    ]
+  },
+  {
+    "atoll": "Noonu",
+    "schools": [
+      "Meyna School",
+      "Kendhikulhudhoo School",
+      "Landhoo School",
+      "Hidhaya School",
+      "Kudafaree School",
+      "Henbadhoo School",
+      "Fodhdhoo School",
+      "Maalhendhoo School",
+      "N. Atoll school",
+      "Maafaru School",
+      "N. Magoodhoo School",
+      "N. Atoll Education Centre",
+      "Lhohee School"
+    ]
+  },
+  {
+    "atoll": "Raa",
+    "schools": [
+      "Inguraidhoo School",
+      "Rasmaadhoo School",
+      "Maduvvaree School",
+      "Alifushi School",
+      "Hulhudhuffaaru School",
+      "Maakurathu School",
+      "Vaadhoo School",
+      "Innamaadhoo School",
+      "Angolhitheemu School",
+      "Ungoofaaru School",
+      "Fainu School",
+      "R. Atoll Education Centre",
+      "R. Atoll school",
+      "Rasgetheemu School",
+      "Kinolhahu School",
+      "Duvaafaru Primary School"
+    ]
+  },
+  {
+    "atoll": "Baa",
+    "schools": [
+      "Thulhaadhoo School",
+      "Hithaadhoo School",
+      "Kudarikilu School",
+      "Kendhoo School",
+      "B. Atoll Education Centre",
+      "B. Atoll School",
+      "Kamadhoo School",
+      "Maalhohu School",
+      "Kihaadhoo School",
+      "Fehendhoo School",
+      "B. Goidhoo School",
+      "Dhonfanu School",
+      "Fulhadhoo School"
+    ]
+  },
+  {
+    "atoll": "Lhaviyani",
+    "schools": [
+      "Madrasathul Ifthithaah",
+      "Olhuvelifushi School",
+      "Lh. Atoll school",
+      "Lh. Atoll Education Centre"
+    ]
+  },
+  {
+    "atoll": "Kaafu",
+    "schools": [
+      "Guraidhoo School",
+      "Maafushi School",
+      "Gaafaru School",
+      "Gulhee School",
+      "Himmafushi School",
+      "Dhiffushi School",
+      "K. Atoll Education Centre",
+      "K. Atoll School",
+      "Huraa School"
+    ]
+  },
+  {
+    "atoll": "Malé / Hulhumalé",
+    "schools": [
+      "Majeediyya School",
+      "Aminiya School",
+      "Iskandhar School",
+      "Centre for Higher Secondary Education Male'",
+      "Jamaluddin School",
+      "Al Madharusathul Arabiyyathul Islaamiyya",
+      "Thaajuddeen school",
+      "Kalaafaanu School",
+      "Muhyiddin School",
+      "Imaduddin School",
+      "Dharumavantha School",
+      "Ghaazee School",
+      "Hiriya School",
+      "Rehendhi School",
+      "Huravee School",
+      "I'zzudhdheen School",
+      "Mohamed Qasim Preschool",
+      "Kaamil Didi Primary School",
+      "S'alaah'udhdheen School",
+      "Centre for Higher Secondary Education Hulhumale'",
+      "Male' Vocational High School",
+      "Shaheed Ali School"
+    ]
+  },
+  {
+    "atoll": "Alifu Alifu",
+    "schools": [
+      "Thoddoo School",
+      "Bodufolhudhoo School",
+      "Mathiveri School",
+      "Aa. Atoll Education Centre",
+      "Ukulhahu School",
+      "Aa. Atoll School",
+      "Himandhoo School",
+      "Maalhohu School"
+    ]
+  },
+  {
+    "atoll": "Alifu Dhaalu",
+    "schools": [
+      "ADh. Atoll School",
+      "Fenfushee School",
+      "Dhigurah School",
+      "ADh. Omadhoo School",
+      "Dhangethee School",
+      "ADh. Dhidhdhoo School",
+      "Hangnaameedhoo School",
+      "ADh. Atoll Education Centre",
+      "Mandhoo School",
+      "Kunburudhoo School"
+    ]
+  },
+  {
+    "atoll": "Vaavu",
+    "schools": [
+      "Rakeedhoo School",
+      "Keyodhoo School",
+      "V. Atoll School",
+      "Thinadhoo School",
+      "V. Atoll Education Centre"
+    ]
+  },
+  {
+    "atoll": "Meemu",
+    "schools": [
+      "Dhiggaru School",
+      "Mulaku School",
+      "Raiymandhoo School",
+      "Naalaafushi School",
+      "M. Maduvvaree School",
+      "Veyvashu School",
+      "M. Atoll Education Center",
+      "M. Atoll School"
+    ]
+  },
+  {
+    "atoll": "Faafu",
+    "schools": [
+      "F. Magoodhoo School",
+      "Dharanboodhoo School",
+      "Bilehdhoo School",
+      "F. Atoll School",
+      "F. Atoll Education Centre"
+    ]
+  },
+  {
+    "atoll": "Dhaalu",
+    "schools": [
+      "Rinbudhoo School",
+      "Bandidhoo School",
+      "Maaenboodhoo School",
+      "Dh. Atoll Education Centre",
+      "Dh. Atoll School",
+      "Hulhudheli School"
+    ]
+  },
+  {
+    "atoll": "Thaa",
+    "schools": [
+      "Vilufushee School",
+      "Kinbidhoo School",
+      "Veymandoo School",
+      "Hirilandhoo School",
+      "Th. Atoll Education Centre",
+      "Burunee School",
+      "Dhiyamigili School",
+      "Gaadhiffushi School",
+      "Th. Omadhoo School",
+      "Kandoodhoo School",
+      "Vandhoo School",
+      "Th. Atoll School",
+      "Madifushi School"
+    ]
+  },
+  {
+    "atoll": "Laamu",
+    "schools": [
+      "Maavashu School",
+      "Hithadhoo School",
+      "Isdhoo School",
+      "Dhanbidhoo School",
+      "Mukurimagu School",
+      "kalaidhoo School",
+      "Mundoo School",
+      "L. Atoll Education Centre",
+      "Kunahandhoo School",
+      "L. Maamendhoo School",
+      "L. Atoll School",
+      "Hamad Bin Khaleefa Al Thani School",
+      "Ihadhdhoo School"
+    ]
+  },
+  {
+    "atoll": "Gaafu Alifu",
+    "schools": [
+      "GA. Atoll School",
+      "GA. Atoll Education Centre",
+      "Dhaandhoo School",
+      "Gemanafushi School",
+      "Kanduhulhudhoo School",
+      "Kondey School",
+      "GA. Maamendhoo School",
+      "GA. Nilandhoo School",
+      "Sulthan Mohamed School"
+    ]
+  },
+  {
+    "atoll": "Gaafu Dhaalu",
+    "schools": [
+      "Vaadhoo Jamaluddeen School",
+      "Hoadedhdhoo School",
+      "Huvadhoo School",
+      "Fiyoaree School",
+      "Nadella School",
+      "Rathafandhoo School",
+      "GDh. Atoll Education Centre",
+      "GDh. Atoll School",
+      "Madaveli School",
+      "Aboobakuru School",
+      "Thinadhoo School"
+    ]
+  },
+  {
+    "atoll": "Gnaviyani",
+    "schools": [
+      "Fuvahmulaku School",
+      "Gn. Atoll Education Centre",
+      "Mohamed Jamaluddin School",
+      "Hafiz Ahmed School"
+    ]
+  },
+  {
+    "atoll": "Seenu",
+    "schools": [
+      "Nooraanee School",
+      "Feydhoo School",
+      "Hithadhoo School",
+      "Maradhoo School",
+      "Sharafuddin School",
+      "S. Atoll School",
+      "Addu High School",
+      "Irushadhiyya School"
+    ]
+  }
+] as const;
